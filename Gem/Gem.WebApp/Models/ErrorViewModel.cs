de4c5ffb1a228d1ed6d151ed11c8e8a197d7aa74
Deivids Dukaļskis <deivids445@inbox.lv>
2022-02-17 11:20:19 +0200

    Removed Gem.Backend and moved Person.cs to Model folder in ASP.NET app

--- Gem/Gem.WebApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Gem.WebApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
