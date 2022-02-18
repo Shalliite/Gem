7ca5168680790334de04902f8cd1b99ba3518361
Deivids Dukaļskis <deivids445@inbox.lv>
2022-02-18 11:05:23 +0200

    Add back deleted things from project template in Views, wwwroot folder

--- Gem/Gem.WebApp/Controllers/HomeController.cs
+++ Gem/Gem.WebApp/Controllers/HomeController.cs
@@ -12,9 +12,21 @@ namespace Gem.WebApp.Controllers
         {
             _logger = logger;
         }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
