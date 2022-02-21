14ce4eb539b61bec87a2c20aaeba71745b23edcf
Deivids Dukaļskis <deivids445@inbox.lv>
2022-02-21 13:53:26 +0200

    Added registration page.
    Related Work Items: #29

---
+++ Gem/Gem.WebApp/Controllers/RegistrationController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Gem.WebApp.Models;
+
+namespace Gem.WebApp.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(RegistrationModel registerDetails)
+        {
+            return View("Index");
+        }
+    }
+}
