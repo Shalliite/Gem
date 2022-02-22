4670de52843d9db6930712fb38e21db9eb3c7f5c
Deivids Dukaļskis <deivids445@inbox.lv>
2022-02-22 11:34:21 +0200

    Storing first sample user to the database

--- Gem/Gem.WebApp/Controllers/RegistrationController.cs
+++ Gem/Gem.WebApp/Controllers/RegistrationController.cs
@@ -1,10 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Gem.WebApp.Models;
+using Gem.WebApp.Services;
+using Gem.WebApp.Migrations;
 
 namespace Gem.WebApp.Controllers
 {
     public class RegistrationController : Controller
     {
+        private UserRepository _userRepository;
+        public RegistrationController(ApplicationDbContext adbc)
+        {
+            _userRepository = new UserRepository(adbc);
+        }
         [HttpGet]
         public IActionResult Index()
         {
@@ -14,6 +21,12 @@ namespace Gem.WebApp.Controllers
         [HttpPost]
         public IActionResult Register(RegistrationModel registerDetails)
         {
+            if (ModelState.IsValid)
+            {
+                MapUsers mapUsers = new MapUsers();
+                User user = mapUsers.Map(registerDetails);
+                _userRepository.Add(user);
+            }
             return View("Index");
         }
     }
