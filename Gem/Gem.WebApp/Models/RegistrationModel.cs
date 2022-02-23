33adcbf7d27f3063e99753ec8316543e7ef72c8a
Deivids Dukaļskis <deivids445@inbox.lv>
2022-02-23 20:20:21 +0200

    1. Added max length validation attribute to FirstName, LastName, MiddleName to be 30 chars long, for E-mail - 50 chars long. 2. Added allowed chars validation attribute for First, Last, Middle name to be (A-z). 3. Added validation messages to be shown for each taxt-box seperately. All messages are in red. 4. Email already registered message will now be visible on top rather than bellow.

--- Gem/Gem.WebApp/Models/RegistrationModel.cs
+++ Gem/Gem.WebApp/Models/RegistrationModel.cs
@@ -10,31 +10,38 @@ namespace Gem.WebApp.Models
     public class RegistrationModel
     {
         [Required]
+        [RegularExpression("[a-zA-Z/'/-]+", ErrorMessage = "Enter valid first name. Allowed characters: (A-z).")]
+        [MaxLength(30, ErrorMessage = "Maximum name length is 30 characters.")]
         [Display(Name = "First Name")]
         public string FirstName { get; set; }
 
+        [RegularExpression("[a-zA-Z/'/-]+", ErrorMessage = "Enter valid middle name. Allowed characters: (A-z).")]
+        [MaxLength(30, ErrorMessage = "Maximum name length is 30 characters.")]
         [Display(Name = "Middle Name")]
         public string? MiddleName { get; set; }
 
         [Required]
+        [RegularExpression("[a-zA-Z/'/-]+", ErrorMessage = "Enter valid last name. Allowed characters: (A-z).")]
+        [MaxLength(30, ErrorMessage = "Maximum name length is 30 characters.")]
         [Display(Name = "Last Name")]
         public string LastName { get; set; }
 
         [Required]
+        [RegularExpression("^[a-zA-Z0-9_\\.-]+@([a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$", ErrorMessage = "Enter valid e-mail address.")]
+        [MaxLength(50, ErrorMessage = "Maximum e-mail length is 50 characters.")]
         [Display(Name = "E-mail")]
-        [RegularExpression("^[a-zA-Z0-9_\\.-]+@([a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$", ErrorMessage = "E-mail is not valid")]
         public string Email { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
-        [StringLength(18, MinimumLength = 8, ErrorMessage = "Password should be 8-18 characters long")]
+        [StringLength(18, MinimumLength = 8, ErrorMessage = "Password should be 8-18 characters long.")]
         public string Password { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
         [Display(Name = "Confirm Password")]
-        [Compare("Password", ErrorMessage = "Passwords do not match")]
+        [Compare("Password", ErrorMessage = "Passwords do not match.")]
         public string ConfirmPassword { get; set; }
     }
 }
