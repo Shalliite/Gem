ee918065b574c3bcce445f9b239e2744301818be
Deivids Dukaļskis <deivids445@inbox.lv>
2022-02-24 13:32:15 +0200

    Added LoginModel class with Email and Password properties. Added validation attributes.
    Related Work Items: #92, #116

---
+++ Gem/Gem.WebApp/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Gem.WebApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
