27065cffed371b03b021bb80e88d8926cc514881
Deivids Dukaļskis <deivids445@inbox.lv>
2022-02-24 13:51:50 +0200

    Created view for loginModel to display basic text-boxes for user input
    Related Work Items: #117

--- Gem/Gem.WebApp/Controllers/LoginController.cs
+++ Gem/Gem.WebApp/Controllers/LoginController.cs
@@ -1,12 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Gem.WebApp.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Gem.WebApp.Controllers
 {
     public class LoginController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(LoginModel loginInfo)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                return View(loginInfo);
+            }
         }
     }
 }
