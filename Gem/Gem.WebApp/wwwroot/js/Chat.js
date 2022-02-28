873602428bb365d1c1bae0cec7aba11ec990817d
Fieldname <martynas.ven@protonmail.com>
2022-02-28 18:08:15 +0200

    test

--- Gem/Gem.WebApp/wwwroot/js/Chat.js
+++ Gem/Gem.WebApp/wwwroot/js/Chat.js
@@ -4,14 +4,22 @@ var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
 
 //Disable the send button until connection is established.
 document.getElementById("sendButton").disabled = true;
-
+let temp = 0;
 connection.on("ReceiveMessage", function (user, message) {
     var li = document.createElement("li");
+    
     document.getElementById("messagesList").appendChild(li);
+    
+    temp++;
+    if (temp > 3) {
+        document.getElementById('messagesList').removeChild(document.getElementById('messagesList').getElementsByTagName('li')[0]);
+    }
+
+    //document.getElementById("messagesList").insertBefore(li);
     // We can assign user-supplied strings to an element's textContent because it
     // is not interpreted as markup. If you're assigning in any other way, you 
     // should be aware of possible script injection concerns.
-    li.textContent = `${user} says ${message}`;
+    li.textContent = `${user}: ${message}`;
 });
 
 connection.start().then(function () {
@@ -23,6 +31,7 @@ connection.start().then(function () {
 document.getElementById("sendButton").addEventListener("click", function (event) {
     var user = document.getElementById("userInput").value;
     var message = document.getElementById("messageInput").value;
+    
     connection.invoke("SendMessage", user, message).catch(function (err) {
         return console.error(err.toString());
     });
